dbfaae5740fb1d520952e92a2952c71215b09464
sarahelshark <sarahelsh183@gmail.com>
2024-02-11 14:22:12 +0100

    feat add pics placeholder

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,11 +15,67 @@ export default {
   <main>
     <!--Jumbotron-->
     <AppJumbotron></AppJumbotron>
+
     <!--Comics section-->
+    <section class="comics">
+      
+      <div class="container">
+        <h3>Current Series</h3>
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+          <div class="col-12 col-md-4 col-lg-2">
+            
+            <div class="card">
+              <img src="https://picsum.photos/200/200" alt="">
+              <h5>lorem</h5>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
     <!--banner-->
   </main>
 </template>
 
-<style>
+<style scoped>
+
+.container{
+  flex-direction: column;
+}
 
 </style>
